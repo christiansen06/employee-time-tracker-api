c85ace614b433d6c88308d1a370e21228f7b32e6
Agustin Christiansen <christiansenagustin@gmail.com>
2026-05-14 11:03:44 -0300

    T2: crear entidad employee y repository

---
+++ employee-time-tracker/src/main/java/com/tuusuario/employee_time_tracker/Model/Employee.java
@@ -0,0 +1,32 @@
+package com.tuusuario.employee_time_tracker.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "employees")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String position;
+
+    @Builder.Default
+    private boolean active = true;
+}
